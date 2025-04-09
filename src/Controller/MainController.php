aed5752826f8e5e80ba8c1a49bbf5a3f58edb179
Tolemak <kamilgggg@gmail.com>
2025-04-09 22:00:36 +0200

    info for dev mode

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,5 +14,15 @@ final class MainController extends AbstractController
     public function index(): Response
     {
         return new RedirectResponse("/file-view/main");
+    }
+
+    #[Route('/phpinfo', name: 'php_info')]
+    public function phpInfo(ParameterBagInterface $params)
+    {
+        if ($params->get("kernel.environment") === "dev") {
+            phpinfo();
+        } else {
+            return new Response("Prod mode is enabled. Please change it to view PHP info.",  Response::HTTP_BAD_REQUEST);
+        }
     }
 }
