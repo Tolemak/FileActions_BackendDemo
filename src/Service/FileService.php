0a87fa83f52c73d6f936af1e536c24d3b9267369
Tolemak <kamilgggg@gmail.com>
2025-04-09 22:01:30 +0200

    file resize service

--- src/Service/FileService.php
+++ src/Service/FileService.php
@@ -2,14 +2,26 @@
 
 namespace App\Service;
 
+use Imagick;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileService
 {
-    function __construct()
+    function __construct(private readonly Filesystem $filesystem)
     {
         // Constructor code here
     }
 
-    function resize(UploadedFile $uploadedFile, int $size) {}
+    function resize(UploadedFile $uploadedFile, int $size): string
+    {
+        $sizePercentage = $size / 100;
+        $temp = tempnam(sys_get_temp_dir(), $uploadedFile->getClientOriginalName());
+        $this->filesystem->dumpFile($temp, $uploadedFile->getContent());
+        $image = new Imagick($temp);
+        $image->scaleImage($image->getImageWidth() * $sizePercentage, $image->getImageHeight() * $sizePercentage);
+        $image->setImageFormat($uploadedFile->getClientOriginalExtension());
+        $image->writeImage($temp);
+        return $temp;
+    }
 }
