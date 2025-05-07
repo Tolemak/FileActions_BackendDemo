c92a9f007678b5f243dc9d41ef87642bc89483d9
Tolemak <kamilgggg@gmail.com>
2025-05-08 00:35:08 +0200

    prepare interface for convert file

--- assets/scripts/sub/appconvert.ts
+++ assets/scripts/sub/appconvert.ts
@@ -1,3 +1,43 @@
 import 'filepond/dist/filepond.min.css';
-import '../app.js';
+import "../app.js";
 import '../../styles/resize.css';
+import { saveBlobAsFile, sweetAlertErrorRequest } from '../utils.js';
+import { SubAppWithFilePond } from './subappwithfilepond.js';
+
+
+class ConvertApp extends SubAppWithFilePond {
+    private _extensionSelectElement: HTMLSelectElement;
+
+    constructor() {
+        super(document.querySelector('input.filepond') as HTMLInputElement);
+        this._extensionSelectElement = document.querySelector("#dest-extension") as HTMLSelectElement;
+    }
+
+    async processFile(fieldName: string, file: File, _metadata, _load, _error, _progress, abort, _transfer, _options): Promise<void> {
+        const destExtension = this._extensionSelectElement.value
+        const formData = new FormData();
+        formData.append(fieldName, file, file.name);
+
+        const request = new Request(`/file/convert/${destExtension}`, {
+            method: "POST",
+            body: formData,
+        });
+
+        fetch(request)
+            .then(res => {
+                this._filePond.removeFiles();
+                if (res.status === 200) {
+                    return res.blob();
+                } else {
+                    sweetAlertErrorRequest(res);
+                    throw new Error("Error");
+                }
+            })
+            .then(blob => {
+                saveBlobAsFile(blob, file);
+            });
+    }
+
+}
+
+new ConvertApp();
--- assets/scripts/utils.ts
+++ assets/scripts/utils.ts
@@ -1,3 +1,5 @@
+import Swal from "sweetalert2";
+
 export function saveBlobAsFile(blob, file) {
     var url = window.URL.createObjectURL(blob);
     var a = document.createElement('a');
@@ -6,4 +8,26 @@ export function saveBlobAsFile(blob, file) {
     document.body.appendChild(a);
     a.click();
     a.remove();
+}
+
+export function sweetAlertErrorRequest(res: Response) {
+    if (res.status === 400) {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Bad Request",
+        });
+    } else if (res.status === 500) {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Internal Server Error",
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Unknown Error",
+        });
+    }
 }
